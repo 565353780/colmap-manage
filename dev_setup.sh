c15bfd1f91ac66303d683ca5bf9a452ba1818210
chLi <565353780@qq.com>
2023-09-24 14:40:56 +0800

    set colmap auto install

--- dev_setup.sh
+++ dev_setup.sh
@@ -11,7 +11,7 @@ if [! -d "/usr/local/bin/colmap"]; then
 	rm -rf build
 	mkdir build
 	cd build
-	cmake .. -G Ninja
+	cmake .. -DCMAKE_CUDA_ARCHITECTURES=86 -G Ninja
 	ninja
 	sudo ninja install
 fi
