818e9d501f1c99ffb68201b3ac525e106189e04f
chLi <565353780@qq.com>
2024-09-03 21:24:26 +0800

    upgrade to support latest colmap

--- setup.sh
+++ setup.sh
@@ -1,15 +1,24 @@
 cd ..
-git clone https://github.com/colmap/colmap.git
+git clone https://github.com/colmap/colmap
+git clone https://github.com/brentyi/ceres-solver
 
 sudo apt-get install git cmake ninja-build build-essential libboost-program-options-dev \
 	libboost-filesystem-dev libboost-graph-dev libboost-system-dev libeigen3-dev libflann-dev \
 	libfreeimage-dev libmetis-dev libgoogle-glog-dev libgtest-dev libsqlite3-dev libglew-dev \
 	qtbase5-dev libqt5opengl5-dev libcgal-dev libceres-dev
 
+cd ceres-solver
+rm -rf build
+mkdir build
+cd build
+cmake .. -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF
+make -j
+sudo make install
+
 pip install opencv-python tqdm
 
 if [ ! -f "/usr/local/bin/colmap" ]; then
-	cd colmap
+	cd ../../colmap
 	rm -rf build
 	mkdir build
 	cd build
