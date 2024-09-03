137723d03ddc822902707760548ed34caffaca86
chLi <565353780@qq.com>
2024-09-03 22:51:22 +0800

    fix colmap run mapper bug

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,23 +1,52 @@
 cd ..
 git clone https://github.com/colmap/colmap
-git clone https://github.com/brentyi/ceres-solver
 
-sudo apt-get install git cmake ninja-build build-essential libboost-program-options-dev \
-	libboost-filesystem-dev libboost-graph-dev libboost-system-dev libeigen3-dev libflann-dev \
-	libfreeimage-dev libmetis-dev libgoogle-glog-dev libgtest-dev libsqlite3-dev libglew-dev \
-	qtbase5-dev libqt5opengl5-dev libcgal-dev libceres-dev
+sudo apt-get install \
+    git \
+    cmake \
+    ninja-build \
+    build-essential \
+    libboost-program-options-dev \
+    libboost-filesystem-dev \
+    libboost-graph-dev \
+    libboost-system-dev \
+    libeigen3-dev \
+    libflann-dev \
+    libfreeimage-dev \
+    libmetis-dev \
+    libgoogle-glog-dev \
+    libgtest-dev \
+    libsqlite3-dev \
+    libglew-dev \
+    qtbase5-dev \
+    libqt5opengl5-dev \
+    libcgal-dev \
+    libceres-dev
 
-cd ceres-solver
+sudo apt install cmake libgoogle-glog-dev libgflags-dev \
+  libatlas-base-dev libeigen3-dev libsuitesparse-dev \
+  libtbb2-dev
+
+if [ ! -d "./ceres-solver-2.0.0/" ]; then
+  wget http://ceres-solver.org/ceres-solver-2.0.0.tar.gz
+  tar zxf ceres-solver-2.0.0.tar.gz
+fi
+sudo rm -r /usr/local/lib/cmake/Ceres
+sudo rm -rf /usr/local/include/ceres
+sudo rm /usr/local/lib/libceres*
+cd ceres-solver-2.0.0
 rm -rf build
 mkdir build
 cd build
-cmake .. -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF
+cmake -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF ..
 make -j
 sudo make install
 
 pip install opencv-python tqdm
 
 if [ ! -f "/usr/local/bin/colmap" ]; then
+  sudo rm -rf /usr/local/include/colmap
+  sudo rm /usr/local/lib/libcolmap*
 	cd ../../colmap
 	rm -rf build
 	mkdir build
